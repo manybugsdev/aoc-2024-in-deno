5ab77f1eaa0f013dfdc6bb43081cc611ee850208
manybugsdev <manybugsdev@users.noreply.github.com>
2024-12-09 23:01:24 +0900

    day9 part1

--- day9/solve.ts
+++ day9/solve.ts
@@ -1,5 +1,17 @@
 const input = await Deno.readTextFile("input.txt");
-const raw = input.split("").map((_, i) =>
-    Array(Number(i)).fill(i % 2 ? "." : `${i}`)
-).flat();
-console.log(raw);
+function calcChecksum(input: string) {
+    const digits = input.split("").map(Number);
+    const ids = Array(Math.ceil(digits.length / 2)).fill(0).flatMap((
+        _,
+        id,
+    ) => Array(digits[2 * id]).fill(id));
+    let position = 0;
+    let sum = 0;
+    for (const [digit, empty] of digits.map((v, i) => [v, i % 2])) {
+        for (const id of ids.splice(empty ? -digit : 0, digit).reverse()) {
+            sum += position++ * id;
+        }
+    }
+    return sum;
+}
+console.log(calcChecksum(input));
