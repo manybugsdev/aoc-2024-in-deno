66b3d864f3e5ddeb0f480de30d89bc3bb21442d0
Manybugs <me@manybugs.dev>
2024-12-10 08:29:06 +0900

    day9

--- day9/solve.ts
+++ day9/solve.ts
@@ -1,9 +1,8 @@
 function calcChecksum(input: string) {
     const digits = input.split("").map(Number);
-    const ids = Array(Math.ceil(digits.length / 2)).fill(0).flatMap((
-        _,
-        id,
-    ) => Array(digits[2 * id]).fill(id));
+    const ids = Array(Math.ceil(digits.length / 2))
+        .fill(0)
+        .flatMap((_, id) => Array(digits[2 * id]).fill(id));
     let position = 0;
     let sum = 0;
     for (const [digit, empty] of digits.map((v, i) => [v, i % 2])) {
@@ -16,35 +15,27 @@ function calcChecksum(input: string) {
 function calcChecksumNoFragment(input: string) {
     const digits = input.split("").map(Number);
     let addr = 0;
-    const stores = Array(digits.length).fill(0).map(
-        (
-            _,
-            i,
-        ) => {
+    const stores = Array(digits.length)
+        .fill(0)
+        .map((_, i) => {
             const s = { id: i % 2 ? -1 : i / 2, stock: digits[i], addr };
             addr += digits[i];
             return s;
-        },
-    );
-    for (
-        const { store, index } of stores.toReversed().map((store, index) => ({
-            store,
-            index: stores.length - index - 1,
-        })).filter(({ store }) => store.id > 0)
-    ) {
-        // find empty
-        const s = stores.slice(0, index).find((s) =>
-            s.id < 0 && s.stock >= store.stock
-        );
-        if (!s) {
+        });
+    const files = stores.filter((s) => s.id >= 0);
+    const frees = stores.filter((s) => s.id < 0);
+    for (const file of files.toReversed()) {
+        const free = frees.find((free) => free.stock >= file.stock);
+        if (!free || free.addr > file.addr) {
             continue;
         }
-        store.addr = s.addr;
-        s.stock -= store.stock;
-        s.addr += store.stock; // n1, n1+1, ..., n2
+        file.addr = free.addr;
+        free.stock -= file.stock;
+        free.addr += file.stock;
     }
-    return stores.filter((s) => s.id > 0).reduce(
-        (sum, { id, stock, addr }) => sum + id * stock * (2 * addr + stock - 1),
+    return files.reduce(
+        (sum, { id, stock, addr }) =>
+            sum + (id * stock * (2 * addr + stock - 1)) / 2,
         0,
     );
 }
