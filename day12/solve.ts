889130104b7cec18c5525707ef922e7690d35244
manybugsdev <manybugsdev@users.noreply.github.com>
2024-12-15 00:14:52 +0900

    solving

--- day12/solve.ts
+++ day12/solve.ts
@@ -1,61 +1,113 @@
-type Memo = Map<number, Map<number, boolean>>;
+type Region = {
+    id: number;
+    elements: RegionElement[];
+};
 
-function memorize(memo: Memo, i: number, j: number) {
-    memo.set(i, (memo.get(i) ?? new Map()).set(j, true));
-}
-
-function exists(memo: Memo, i: number, j: number) {
-    return !!memo.get(i)?.get(j);
-}
+type RegionElement = {
+    i: number;
+    j: number;
+    plant: string;
+    fence: number;
+};
 
 function around(i: number, j: number) {
     return [[i - 1, j], [i, j + 1], [i + 1, j], [i, j - 1]];
 }
 
-function perimeter(map: string[][], i: number, j: number, memo: Memo): number {
-    if (exists(memo, i, j)) {
-        return 0;
-    }
-    const ch = map[i]?.[j];
-    if (!ch) {
-        return 0;
-    }
-    memorize(memo, i, j);
-    const sames = around(i, j).filter(([i, j]) => map[i]?.[j] === ch);
-    return sames.reduce(
-        (sum, [i, j]) => sum + perimeter(map, i, j, memo),
-        4 - sames.length,
+function getFence(map: string[][], i: number, j: number) {
+    const plant = map[i][j];
+    return around(i, j).reduce(
+        (fence, [i, j], k) =>
+            fence + (map[i]?.[j] === plant ? 0 : Math.pow(2, k)),
+        0,
     );
 }
 
-function area(
+function getPositionsSameRegion(
     map: string[][],
     i: number,
     j: number,
-    memo: Memo,
-): number {
-    if (exists(memo, i, j)) {
-        return 0;
+    plant = map[i]?.[j],
+    visited = map.map((r) => r.map(() => false)),
+): [number, number][] {
+    if (map[i]?.[j] !== plant || visited[i]?.[j]) {
+        return [];
     }
-    const ch = map[i]?.[j];
-    if (!ch) {
-        return 0;
+    visited[i][j] = true;
+    return [
+        [i, j],
+        ...around(i, j).map(([i, j]) =>
+            getPositionsSameRegion(map, i, j, plant, visited)
+        ).flat(),
+    ];
+}
+
+function getRegions(map: string[][]): Region[] {
+    let regionId = 1;
+    const visited = map.map((r) => r.map(() => false));
+    return map.map((r, i) =>
+        r.flatMap((plant, j) => {
+            if (visited[i][j]) {
+                return [];
+            }
+            return [{
+                id: regionId++,
+                elements: getPositionsSameRegion(map, i, j).map(([i, j]) => {
+                    visited[i][j] = true;
+                    return ({
+                        i,
+                        j,
+                        plant,
+                        fence: getFence(map, i, j),
+                    });
+                }),
+            }];
+        })
+    ).flat();
+}
+
+function getArea(region: Region) {
+    return region.elements.length;
+}
+function getPerimeter(region: Region) {
+    return region.elements.reduce((p, { fence }) => p + countBits(fence), 0);
+}
+
+function countBits(fence: number) {
+    let c = 0;
+    while (fence) {
+        c += fence & 0b0001;
+        fence >>= 1;
     }
-    memorize(memo, i, j);
-    return around(i, j).filter(([i, j]) => map[i]?.[j] === ch).reduce(
-        (sum, [i, j]) => sum + area(map, i, j, memo),
-        1,
+    return c;
+}
+
+function getTotalPrice(input: string): number {
+    const map = input.split("\n").map((r) => r.split(""));
+    const regions = getRegions(map);
+    return regions.reduce(
+        (t, region) => t + getArea(region) * getPerimeter(region),
+        0,
     );
 }
 
-function priceTotal(input: string): number {
+function getSide(region: Region) {
+    const elements = region.elements;
+    const checked: RegionElement[] = [];
+    for (const el of elements) {
+        const ars = checked.filter(({ i, j }) =>
+            around(el.i, el.j).find(([ai, aj]) => ai == i && aj == j)
+        );
+        // solving
+    }
+    return checked.reduce((s, { fence }) => s + countBits(fence), 0);
+}
+
+function getDiscountedTotalPrice(input: string): number {
     const map = input.split("\n").map((r) => r.split(""));
-    const am: Memo = new Map();
-    const pm: Memo = new Map();
-    return map.flatMap((line, i) => line.map((_, j) => ({ i, j }))).reduce(
-        (sum, { i, j }) => (
-            sum + area(map, i, j, am) * perimeter(map, i, j, pm)
-        ),
+    const regions = getRegions(map);
+    return regions.reduce(
+        (t, region) => t + getArea(region) * getSide(region),
         0,
     );
 }
@@ -63,6 +115,33 @@ function priceTotal(input: string): number {
 if (import.meta.main) {
     const input = await Deno.readTextFile("input.txt");
     console.log(
-        `priceTotal: ${priceTotal(input)}`,
+        `TotalPrice: ${
+            getTotalPrice(`RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`)
+        }`,
     );
+    /*
+    console.log(
+        `priceTotalDiscounted: ${
+            priceTotalDiscounted(`RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`)
+        }`,
+    );
+    */
 }
