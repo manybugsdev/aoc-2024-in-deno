a6d9eedb241a361abc4472e552c1c245ee19fe30
manybugsdev <manybugsdev@users.noreply.github.com>
2024-12-15 23:22:01 +0900

    day13

--- day13/solve.ts
+++ day13/solve.ts
@@ -47,7 +47,25 @@ function getFewestTokens(input: string): number {
     );
 }
 
+function getActualFewestTokens(input: string): number {
+    return input.split("\n\n").map((text) => text.split("\n")).map((
+        [at, bt, pt],
+    ) => ({
+        ax: Number(at.split(",")[0].slice(-3)),
+        ay: Number(at.split(",")[1].slice(-3)),
+        bx: Number(bt.split(",")[0].slice(-3)),
+        by: Number(bt.split(",")[1].slice(-3)),
+        px: 10000000000000 + Number(pt.split(",")[0].match(/[0-9]+/)?.[0]),
+        py: 10000000000000 + Number(pt.split(",")[1].match(/[0-9]+/)?.[0]),
+    })).reduce(
+        (sum, { ax, ay, bx, by, px, py }) =>
+            sum + getFewestToken(ax, ay, bx, by, px, py),
+        0,
+    );
+}
+
 if (import.meta.main) {
     const input = await Deno.readTextFile("input.txt");
     console.log(`FewestTokens: ${getFewestTokens(input)}`);
+    console.log(`ActualFewestTokens: ${getActualFewestTokens(input)}`);
 }
