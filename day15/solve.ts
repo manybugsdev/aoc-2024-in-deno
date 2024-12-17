7baaceaa1c938087f2f747512e09cd4bdedd930b
manybugsdev <manybugsdev@users.noreply.github.com>
2024-12-17 20:08:07 +0900

    bug

--- day15/solve.ts
+++ day15/solve.ts
@@ -11,6 +11,31 @@ function parseInput(
     return { map, directions };
 }
 
+function parseInputTwice(
+    input: string,
+): { map: string[][]; directions: Direction[] } {
+    const [mapString, movesString] = input.split("\n\n");
+    const map = mapString.split("\n").map((line) =>
+        line.split("").map((ch) => {
+            switch (ch) {
+                case "#":
+                    return "##";
+                case ".":
+                    return "..";
+                case "O":
+                    return "[]";
+                case "@":
+                    return "@.";
+            }
+            return "";
+        }).join("").split("")
+    );
+    const directions = movesString.split("").filter(
+        (ch) => ch !== "\n",
+    ) as Direction[];
+    return { map, directions };
+}
+
 function nextPosition(
     x: number,
     y: number,
@@ -79,8 +104,74 @@ function printMap(map: string[][]): void {
     console.log(map.map((row) => row.join("")).join("\n"));
 }
 
+function runStepTwice(
+    map: string[][],
+    direction: Direction,
+    x: number,
+    y: number,
+): [string[][], boolean] {
+    const mark = map[y][x];
+    if (mark === "#" || mark === ".") {
+        return [map, mark === "."];
+    }
+    if (["[", "]"].includes(mark) && ["^", "v"].includes(direction)) {
+        const np = nextPosition(x, y, direction);
+        const dx = mark === "[" ? 1 : -1;
+        let s1: boolean, s2: boolean;
+        [map, s1] = runStepTwice(map, direction, np.x, np.y);
+        [map, s2] = runStepTwice(
+            map,
+            direction,
+            np.x + dx,
+            np.y,
+        );
+        if (!s1 || !s2) {
+            return [map, false];
+        }
+        map[y][x] = ".";
+        map[y][x + dx] = ".";
+        map[np.y][np.x] = mark;
+        map[np.y][np.x + dx] = mark === "[" ? "]" : "[";
+        return [map, true];
+    }
+    const np = nextPosition(x, y, direction);
+    [map] = runStepTwice(map, direction, np.x, np.y);
+    const nmark = map[np.y][np.x];
+    if (nmark !== ".") {
+        return [map, false];
+    }
+    map[y][x] = ".";
+    map[np.y][np.x] = mark;
+    return [map, true];
+}
+
+function runTwice(map: string[][], directions: Direction[]): string[][] {
+    for (const direction of directions) {
+        const { x, y } = getRobotPosition(map);
+        [map] = runStepTwice(map, direction, x, y);
+    }
+    return map;
+}
+
+function getSumOfGPSTwice(map: string[][]): number {
+    return map.reduce(
+        (sum, r, y) =>
+            r.reduce(
+                (sum, mark, x) => sum + (mark === "[" ? 100 * y + x : 0),
+                sum,
+            ),
+        0,
+    );
+}
+
 if (import.meta.main) {
-    let { map, directions } = parseInput(await Deno.readTextFile("input.txt"));
+    const text = await Deno.readTextFile("input.txt");
+    let { map, directions } = parseInput(text);
     map = run(map, directions);
     console.log(`SumOfGPS: ${getSumOfGPS(map)}`);
+    ({ map, directions } = parseInputTwice(
+        text,
+    ));
+    map = runTwice(map, directions);
+    console.log(`SumOfGPSTwice: ${getSumOfGPSTwice(map)}`);
 }
